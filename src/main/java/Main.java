c31c9e5e586275c8fee6511fbb9ce852dc29811e
Denis Gubanov <gdav205@gmail.com>
2023-06-05 13:39:55 +0300

    Написан код, проверяющий существует ли файл, по указаному пути.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,39 @@
+import java.io.File;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length())
-        ;
+
+        int i = 0;
+
+        while (i >= 0){
+
+            System.out.println("Введите полный путь к файлу:");
+
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+
+            boolean isDirectory = file.isDirectory();
+
+            boolean fileExists = file.exists();
+
+            if (fileExists == true && isDirectory == true){
+                System.out.println("Введен путь к директории, а не к файлу. Попробуйде снова");
+                continue;
+            }
+            if (fileExists == true && isDirectory == false){
+                i++;
+                System.out.println("Путь указан верно.");
+                System.out.println("Это файл номер "+i);
+                continue;
+            }
+            else {
+                System.out.println("Указанный файл не существует.");
+                continue;
+            }
+        }
+
+
     }
 }
